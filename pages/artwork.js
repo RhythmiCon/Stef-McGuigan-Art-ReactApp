d5805f8fdaf2e1a88e0901a8ad43cfb772161bca
RhythmiCon <conormcguigan.code@gmail.com>
2022-02-12 10:29:47 +0000

    colour scheme altered on artwork page

--- pages/artwork.js
+++ pages/artwork.js
@@ -2,5 +2,9 @@ import AllContent from "../components/AllContent/AllContent";
 import css from "../styles/Artwork.module.css";
 
 export default function Artwork() {
-   return <AllContent />;
+   return (
+      <div className={css.all_content}>
+         <AllContent />;
+      </div>
+   );
 }
